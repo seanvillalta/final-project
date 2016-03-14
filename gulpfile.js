fd37fb170bdcf7a408b8c3099084e9f80b61efb3
Jay Johnson <Jay@Jays-MBP.attlocal.net>
2016-03-14 13:06:41 -0700

    Replace index.html, scss, gulpfile.js and config.rb files from Jay's final-project-gulp repo

--- gulpfile.js
+++ gulpfile.js
@@ -3,76 +3,69 @@
 // ////////////////////////////////////////////////
 
 var gulp        = require('gulp'),
+    compass     = require('gulp-compass'),
     uglify      = require('gulp-uglify'),
     concat      = require('gulp-concat'),
     cleanCSS    = require('gulp-clean-css'),
     sourcemaps  = require('gulp-sourcemaps'),
-    sass        = require('gulp-sass'),
-    browserSync = require('browser-sync').create(),
+    browserSync = require('browser-sync'),
+    reload      = browserSync.reload,
     imagemin    = require('gulp-imagemin'),
+    size        = require('gulp-size');
     rename      = require('gulp-rename');
 
 // ////////////////////////////////////////////////
 // Scripts Task
 // ////////////////////////////////////////////////
-gulp.task('scripts', function() {
-    gulp.src(['node_modules/jquery/dist/jquery.js',
-            'node_modules/bootstrap/dist/js/bootstrap.min.js'
-        ])
-        .pipe(uglify())
-        .pipe(concat('scripts.min.js'))
-        .pipe(gulp.dest('js'))
-        .pipe(browserSync.stream());
+gulp.task('scripts', function(){
+    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js'])
+    .pipe(uglify())
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('js'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
 // ////////////////////////////////////////////////
-// Image Optimization Tasks
+// Styles Tasks
 // ////////////////////////////////////////////////
-gulp.task('image', function() {
+gulp.task('compass', function(){
+    gulp.src('scss/*.scss')
+        .pipe(compass({
+            sourcemap: true,
+            debug : true,
+            style: 'compressed',
+            css: 'css',
+            sass: 'scss'
+        }))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({stream:true}));
+});
+
+// ////////////////////////////////////////////////
+// Image Optimization and Gulp-Size Tasks
+// ////////////////////////////////////////////////
+gulp.task('image', function(){
     gulp.src('images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('images-min'));
+    .pipe(imagemin())
+    .pipe(size())
+    .pipe(gulp.dest('images-min'));
 });
 
 // ////////////////////////////////////////////////
 // Browser-Sync Tasks
 // ////////////////////////////////////////////////
-gulp.task('serve', ['sass'], function() {
-
-    browserSync.init({
-        server: "./"
+gulp.task('serve', function(){
+    browserSync({
+        server:{
+            baseDir: "./"
+        }
     });
-
-    // gulp.watch('app/js/**/*.js', ['scripts']);
-    gulp.watch("scss/*.scss", ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
-});
-
-// ////////////////////////////////////////////////
-// Sass Tasks
-// ////////////////////////////////////////////////
-gulp.task('sass', function() {
-    return gulp.src("scss/*.scss")
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(gulp.dest("css"))
-        .pipe(sourcemaps.write())
-        .pipe(browserSync.stream());
-});
-
-// ////////////////////////////////////////////////
-// CSS-MIN Tasks
-// ////////////////////////////////////////////////
-gulp.task('css-min', function() {
-    return gulp.src("scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("css"))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest("css"));
+    // gulp.watch('js/**/*.js', ['scripts']);
+    gulp.watch('scss/*.scss', ['compass']);
+    gulp.watch('**/*.html').on('change', browserSync.reload);
 });
 
 // ////////////////////////////////////////////////
 // Default Task
 // ////////////////////////////////////////////////
-gulp.task('default', ['serve', 'image', 'scripts', 'css-min']);
+gulp.task('default', ['scripts', 'compass', 'image', 'serve']);
